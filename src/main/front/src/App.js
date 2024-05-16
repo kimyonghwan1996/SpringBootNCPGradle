085c1687481a1a5d03ddf9cda8e6f5e67d4a4420
kimyonghwan1996 <137673370+kimyonghwan1996@users.noreply.github.com>
2024-05-16 12:53:33 +0900

    1

---
+++ src/main/front/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div>
+      <h1>Spring Boot + React + NCP Test</h1>
+
+      <img src='./logo192.png' alt='logo' />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/test/java/com/example/springbootncpgradle/SpringBootNcpGradleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootncpgradle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootNcpGradleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
